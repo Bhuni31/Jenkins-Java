6cd9234f12289f06be0bfb49e89347938d64c0e8
Bhuni31 <bhuni31@gmail.com>
2025-02-21 07:45:27 +0530

    file modified

--- Documents/sample.java
+++ Documents/sample.java
@@ -1,10 +1,7 @@
-package sample;
-
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("I am learning Java.");
+    System.out.println("It is awesome!");
+  }
 }
